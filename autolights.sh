239972cacbcd8445b5027ee99c10c31c0f002951
Samuel Hilson <samuel.hilson@wizards.com>
2023-06-21 15:49:18 -0500

    Updating to have a delay befor turning off light

--- autolights.sh
+++ autolights.sh
@@ -1,30 +1,86 @@
 #!/bin/bash
+script_path=$(dirname "$(realpath "$0")")
 
-# Begin looking at the system log via the steam sub-command. Using a --predicate and filtering by the correct and pull out the camera event 
-log stream --predicate 'subsystem == "com.apple.UVCExtension" and composedMessage contains "Post PowerLog"' | while read line; do
+config_file="$script_path/config.sh"
+
+# Check if the configuration file exists
+if [ ! -f "$config_file" ]; then
+  echo "Error: Configuration file '$config_file' not found."
+  exit 1
+fi
+
+# Load the configuration file
+source "$config_file"
+
+# Validate brightness value
+if (( brightness < 1 || brightness > 100 )); then
+  echo "Error: Invalid brightness value in the configuration file."
+  exit 1
+fi
+
+# Validate temperature value
+if (( temperature < 143 || temperature > 344 )); then
+  echo "Error: Invalid temperature value in the configuration file."
+  exit 1
+fi
+
+# Function to turn on the lights
+turn_on_lights() {
+  local ip=$1
+  local brightness=$2
+  local temperature=$3
   
-  # The camera start event has been caught and is set to 'On', turn the light on
+  echo -e "\nCamera is activated, turn on the lights."
+  curl --location --request PUT "http://$ip:9123/elgato/lights" \
+    --header 'Content-Type: application/json' \
+    --data-raw "{\"lights\":[{\"brightness\":$brightness,\"temperature\":$temperature,\"on\":1}]}"
+}
+
+# Function to turn off the lights
+turn_off_lights() {
+  local ip=$1
+  
+  echo -e "\nCamera is deactivated, turn off the lights."
+  curl --location --request PUT "http://$ip:9123/elgato/lights" \
+    --header 'Content-Type: application/json' \
+    --data-raw "{\"lights\":[{\"on\":0}]}"
+}
+
+# Turn off the lights to normalize the state
+power_state=$(log show --predicate 'subsystem == "com.apple.UVCExtension" and composedMessage contains "Post PowerLog"' --last 1h | awk '/VDCAssistant_Power_State/ {state = $NF} END {print state}')
+if [ "$power_state" = "Off;" ]; then
+  for ip in "${ip_addresses[@]}"; do
+      turn_off_lights "$ip"
+  done
+fi
+if [ "$power_state" = "On;" ]; then
+  for ip in "${ip_addresses[@]}"; do
+      turn_on_lights "$ip" "$brightness" "$temperature"
+  done
+fi
+
+
+# Begin looking at the system log via the steam sub-command.
+log stream --predicate 'subsystem == "com.apple.UVCExtension" and composedMessage contains "Post PowerLog"' | while read -r line; do
+  # Check if the camera start event has been caught and is set to 'On'
   if echo "$line" | grep -q "= On"; then
-	
-	echo "Camera has been activated, turn on the light."
-		
-	# Light 1 - change IP to your first lights IP
-	curl --location --request PUT 'http://192.168.86.40:9123/elgato/lights' --header 'Content-Type: application/json' --data-raw '{"lights":[{"brightness":40,"temperature":162,"on":1}],"numberOfLights":1}'
-
-	#Light 2 (remove if you only have 1 light), change IP to your second light
-	curl --location --request PUT 'http://192.168.86.38:9123/elgato/lights' --header 'Content-Type: application/json' --data-raw '{"lights":[{"brightness":40,"temperature":162,"on":1}],"numberOfLights":1}'
-
+    for ip in "${ip_addresses[@]}"; do
+      turn_on_lights "$ip" "$brightness" "$temperature"
+    done
   fi
 
-  # If we catch a camera stop event, turn the light off.
-  if echo "$line" | grep -q "= Off"; then
-	echo "Camera shut down, turn off the light."
-	
-	#Light 1 - set to off
-	curl --location --request PUT 'http://192.168.86.40:9123/elgato/lights' --header 'Content-Type: application/json' --data-raw '{"lights":[{"brightness":40,"temperature":162,"on":0}],"numberOfLights":2}'
+# Check if we catch a camera stop event
+if echo "$line" | grep -q "= Off"; then
+  # Sleep for 3 seconds
+  sleep 3
+  # Check if the camera is still off
+  power_state=$(log show --predicate 'subsystem == "com.apple.UVCExtension" and composedMessage contains "Post PowerLog"' --last 1m | awk '/VDCAssistant_Power_State/ {state = $NF} END {print state}')
 
-	#Light 2 - set to off
-	curl --location --request PUT 'http://192.168.86.38:9123/elgato/lights' --header 'Content-Type: application/json' --data-raw '{"lights":[{"brightness":40,"temperature":162,"on":0}],"numberOfLights":2}'
-
+  # Camera is still off after 3 seconds
+  if [ "$power_state" = "Off;" ]; then
+    for ip in "${ip_addresses[@]}"; do
+      turn_off_lights "$ip"
+    done
   fi
+fi
 done
