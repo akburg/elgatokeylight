a8e7cc59f27ccc8fad680f945a20afa4ea82f068
Akbur Ghafoor <101948448+akburg@users.noreply.github.com>
2024-02-20 19:23:51 +0000

    Sonoma (v14.x.x) compatibility
    It seems like the logs in Sonoma (Mac OS v14.x.x) behave differently when the camera stream is on or off.  Updated the script so it looks for the 'AVFCapture' entry in the log and detects if the camera is on using 'AVCaptureSession_Tundra startRunning' and off using 'AVCaptureSession_Tundra stopRunning' entries which appear to be reliably consistent for both the built-in cam or a 3rd party cam.

---
+++ autolights_sonoma.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Begin looking at the system log via the steam sub-command. Using a --predicate and filtering by the correct and pull out the camera event 
+
+echo "Monitoring camera stream."
+
+log stream --predicate 'composedMessage contains "AVFCapture"' | while read line; do
+  
+  # The camera start event has been caught and is set to 'On', turn the light on
+  if echo "$line" | grep -qE "AVCaptureSession_Tundra startRunning"; then
+	
+	echo "Camera has been activated, turn on the light."
+		
+	# Light 1 - change IP to your first lights IP
+	curl --location --request PUT 'http://192.168.50.192:9123/elgato/lights' --header 'Content-Type: application/json' --data-raw '{"lights":[{"brightness":40,"temperature":162,"on":1}],"numberOfLights":1}'
+
+	#Light 2 (remove if you only have 1 light), change IP to your second light
+	#curl --location --request PUT 'http://192.168.86.38:9123/elgato/lights' --header 'Content-Type: application/json' --data-raw '{"lights":[{"brightness":40,"temperature":162,"on":1}],"numberOfLights":1}'
+
+  fi
+
+  # If we catch a camera stop event, turn the light off.
+  if echo "$line" | grep -qE "AVCaptureSession_Tundra stopRunning"; then
+	echo "Camera shut down, turn off the light."
+	
+	#Light 1 - set to off
+	curl --location --request PUT 'http://192.168.50.192:9123/elgato/lights' --header 'Content-Type: application/json' --data-raw '{"lights":[{"brightness":40,"temperature":162,"on":0}],"numberOfLights":2}'
+
+	#Light 2 - set to off
+	#curl --location --request PUT 'http://192.168.86.38:9123/elgato/lights' --header 'Content-Type: application/json' --data-raw '{"lights":[{"brightness":40,"temperature":162,"on":0}],"numberOfLights":2}'
+
+  fi
+done
